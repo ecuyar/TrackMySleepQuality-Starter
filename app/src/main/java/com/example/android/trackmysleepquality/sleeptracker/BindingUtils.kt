3f6126cf4b3119e0443c395a5d8eda532c407bf5
Enes Can UYAR <uyar.enescan@gmail.com>
2021-04-11 18:52:54 +0300

    Created binding adapter

---
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -0,0 +1,37 @@
+package com.example.android.trackmysleepquality.sleeptracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
+import com.example.android.trackmysleepquality.database.SleepNight
+
+//Each function is called "extension function"
+//This function will be your adapter for calculating and formatting the sleep duration.
+//To tell data binding about this binding adapter, annotate the function with @BindingAdapter.
+//This function is the adapter for the sleepDurationFormatted attribute, so pass sleepDurationFormatted as an argument to @BindingAdapter.
+@BindingAdapter("sleepDurationFormatted")
+fun TextView.setSleepDurationFormatted(item: SleepNight) {
+    text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, context.resources)
+}
+
+@BindingAdapter("setSleepQualityString")
+fun TextView.setSleepQualityString(item: SleepNight) {
+    text = convertNumericQualityToString(item.sleepQuality, context.resources)
+}
+
+@BindingAdapter("setSleepImage")
+fun ImageView.setSleepImage(item: SleepNight) {
+    setImageResource(when (item.sleepQuality) {
+        0 -> R.drawable.sleep_0
+        1 -> R.drawable.sleep_1
+        2 -> R.drawable.sleep_2
+        3 -> R.drawable.sleep_3
+        4 -> R.drawable.sleep_4
+        5 -> R.drawable.sleep_5
+        else -> R.drawable.sleep_0
+    })
+}
+
